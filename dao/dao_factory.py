784ed4af54f44b51853123c642bb187e78b85904
SainTurDaY27 <poom52344@gmail.com>
2022-04-27 14:43:52 +0700

    Fix import in dao_factory.

--- dao/dao_factory.py
+++ dao/dao_factory.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from game_dao import GameDao
-from publisher_dao import PublisherDao
+from .game_dao import GameDao
+from .publisher_dao import PublisherDao
 
 
 class DaoFactory:

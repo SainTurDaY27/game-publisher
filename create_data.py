6e5ab7dbeb5cca4b92a3668598fd834cc175fa8d
SainTurDaY27 <poom52344@gmail.com>
2022-04-28 00:05:12 +0700

    Modify file.

--- create_data.py
+++ create_data.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 
 from model import GameData, Publisher
 import model
 
-# base = declarative_base()
 engine = create_engine('sqlite:///game_data.db')
 model.base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
